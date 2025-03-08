b90946dfe8c8b9a665e32bc5e9c70deed4e5c8a4
kpurohit009 <kpurohit009@gmail.com>
2025-03-08 21:53:16 +0530

    modification

--- routes/review.js
+++ routes/review.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const { reviewSchema } = require("../scema.js");
+const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 
